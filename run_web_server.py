a70b5bee7032fa482298f259a40a9712b719fd1a
daocongtuyen2x <daocongtuyen2x@gmail.com>
2022-05-26 20:01:09 +0700

    add files

--- run_web_server.py
+++ run_web_server.py
@@ -1,5 +1,5 @@
 # import the necessary packages
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import img_to_array
 from keras.applications import imagenet_utils
 from PIL import Image
 import numpy as np
